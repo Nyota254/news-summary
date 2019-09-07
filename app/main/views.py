5e87de6954f7f44147f67b60b3b89488c189c490
Nyota254 <knyota66@gmail.com>
2019-09-07 19:14:32 +0300

    set up the view for news sources

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-#from ..requests import
-#from ..models import 
+from ..requests import get_sources
+from ..models import News_article,News_source 
 
 @main.route('/')
 def index():
@@ -9,5 +9,8 @@ def index():
     Home page function returns news sources
     '''
 
+    news_sources = get_sources()
+
+
     title = "Welcome to news Summary a collection of all the news sources"
-    return render_template('index.html',title = title)
+    return render_template('index.html',title = title, sources = news_sources)
