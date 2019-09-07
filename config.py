669c115a3dd71c0cb6343fe33a539d7c27c13420
Nyota254 <knyota66@gmail.com>
2019-09-07 23:21:41 +0300

    bug fix of quotes on base url

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     Class for configurations to be added during run-time
     '''
     
-    NEWS_API_BASE_URL = https://newsapi.org/v2/sources?apiKey={}
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
